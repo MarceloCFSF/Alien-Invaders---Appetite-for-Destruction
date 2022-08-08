9ba95bcd05f74c0894fc39de2626d6aff69c1238
MedusAsudeM <109161214+MedusAsudeM@users.noreply.github.com>
2022-08-07 23:45:08 -0300

    Atualização Bt

---
+++ Assets/Scenes/Level.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour
+{
+    public Button Button;
+    public Button BtBack;
+
+
+
+    void Start()
+    {
+        Button btn = BtBack.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+        Button btn2 = Button.GetComponent<Button>();
+        btn2.onClick.AddListener(TaskOnClick2);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    void TaskOnClick2()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
+
+
+
+}
