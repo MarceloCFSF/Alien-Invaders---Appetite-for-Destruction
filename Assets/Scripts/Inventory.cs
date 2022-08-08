d749227520520166cd7711882b3f5badfe4f4d48
MarceloCFSF <marcelo.cfsf@gmail.com>
2022-08-07 22:05:48 -0300

    Movimentação e coleta de items

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public int Plants { get; private set; }
+
+    public void PlantCollected()
+    {
+        Plants++;
+    }
+}
