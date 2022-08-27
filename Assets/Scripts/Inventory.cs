75c13c4f29b34ea5e3d0048e32bce685fe5ad34d
MarceloCFSF <marcelo.cfsf@gmail.com>
2022-08-27 17:21:08 -0300

    Add Tempo

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -6,13 +6,34 @@ using UnityEngine.SceneManagement;
 public class Inventory : MonoBehaviour
 {
     public int Plants { get; private set; }
+    private int goal;
+
+    public void Start() {
+        goal = FindGameObjectsWithLayer(LayerMask.NameToLayer("Collectable"));
+        Debug.Log("Total Plants: " + goal);
+
+    }
 
     public void PlantCollected()
     {
         Plants++;
         Debug.Log("plants: " + Plants);
-        if (Plants == 5) {
+        if (Plants == goal) {
             SceneManager.LoadScene("Intro");
         }
+    }
+
+    int FindGameObjectsWithLayer (int layer) { 
+        GameObject[] goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[]; 
+        Debug.Log("GameObjects: " + goArray.Length);
+        Debug.Log("Layer: " + layer);
+        List<GameObject> goList = new List<GameObject>(); 
+        for (int i = 0; i < goArray.Length; i++) { 
+            if (goArray[i].layer == layer) { 
+                goList.Add(goArray[i]); 
+            }
+            Debug.Log("Layer Obj: " + goArray[i].layer);
+        }
+        return goList.Count; 
     }
 }
---
+++ Assets/Scripts/TimeHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; 
+
+public class TimeHandler : MonoBehaviour
+{
+    public float Countdown;
+    public Text countdownText;
+
+    // Update is called once per frame
+    void Update() {
+        Countdown = Mathf.Clamp(Countdown - Time.deltaTime, 0, 100);
+        countdownText.text = Mathf.CeilToInt(Countdown).ToString();
+        
+        if (Countdown <= 0) {
+            SceneManager.LoadScene("Intro");
+        }
+
+    }
+}
