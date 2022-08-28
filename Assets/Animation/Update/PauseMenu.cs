1eed84e6235df3ca19a5330d893959812bc3cb5e
MedusAsudeM <109161214+MedusAsudeM@users.noreply.github.com>
2022-08-28 18:35:23 -0300

    Animações, Countdown e Pause

---
+++ Assets/Animation/Update/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+
+{
+
+    [SerializeField] private GameObject pauseMenu;
+
+    public void Pause()
+    {
+        //ativa o pause
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        //desativa o pause
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    //public void Home(int sceneID)
+    public void Home()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+
+
+}
